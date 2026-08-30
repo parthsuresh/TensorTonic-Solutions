fdaaea9e9aff3845e760c1aa2a3a8cbf03082cfc
Parth Suresh <parthsuresh96@gmail.com>
2026-08-29 21:24:04 -0700

    Solve autograd-l02-build-expression-graph

---
+++ study-plans/autograd-from-scratch/autograd-l02-build-expression-graph/autograd-l02-build-expression-graph.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def build_expression_graph(leaves, operations):
+    """
+    Returns: node records in creation order and the final node ID
+    """
+    output = []
+
+    id_to_val = {}
+    
+    for leaf in leaves:
+        output.append({
+          "grad": 0.0, "op": "", "parents": [], **leaf  
+        })
+        id_to_val[leaf["id"]] = leaf["data"]
+
+    def compute_data(left_val, right_val, op):
+        if op == "*":
+            return left_val * right_val
+        elif op == "+":
+            return left_val + right_val
+        else:
+            raise ValueError("Unsupported operation!")
+    
+    for operation in operations:
+        data = compute_data(id_to_val[operation["left"]], id_to_val[operation["right"]], operation["op"])
+        id_to_val[operation["id"]] = data
+        output.append(
+            {
+                "id": operation["id"],
+                "data": data,
+                "grad": 0.0,
+                "op": operation["op"],
+                "parents": [operation["left"], operation["right"]]
+            }
+        )
+
+    return (output, output[-1]["id"])
